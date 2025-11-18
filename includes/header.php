6109c1225aa2d1024db2c2476609c2cde94a5be3
TBDC <sio.bureauducolombier@gmail.com>
2025-11-18 15:05:23 +0100

    first commit

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<header class="site-header">
+    <div class="logo">
+        <a href="#">MonLogo</a>
+    </div>
+    <nav class="main-nav">
+        <ul>
+            <li><a href="#">Accueil toto</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="cta">
+        <a href="#" class="btn-primary">Se connecter</a>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php require_once "includes/header.php"; ?>
+
+    <main>
+        <section class="hero-section">
+            <div>
+                 <h1>Faites de vos idées des projets, et de l'argent...</h1>
+                <div>Envoyez-moi votre idée de projet, et je vous la réalise en deux semaines (mensonge garanti).</div>
+            </div>
+            <img src="img/thomas-bdc.jpg" alt="Photo de profile Thomas">
+        </section>
+        <section class="projects">
+
+        </section>
+        <section class="skills">
+
+        </section>
+        <section class="references">
+
+        </section>
+    </main>
+
+    <?php include_once "includes/footer.php"; ?>
+
+</body>
+</html>
